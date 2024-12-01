93fa01f1943fe3d6ecdc71d8ea608a34107e3f59
john thornton <jb087011@gmail.com>
2024-11-30 21:27:06 -0500

    Implement $.ajax!

--- itis3135/scripts/jsonimpl.js
+++ itis3135/scripts/jsonimpl.js
@@ -1,17 +1,32 @@
 // script.js
-document.addEventListener("DOMContentLoaded", () => {
-  // Fetch JSON data
-  const navHeader = document.getElementById("headernav");
+// document.addEventListener("DOMContentLoaded", () => {
+//   // Fetch JSON data
+//   const navHeader = document.getElementById("headernav");
 
-  fetch("./components/navbar.json")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      console.log(navHeader);
-      
-      data.forEach((post) => {
-        navHeader.insertAdjacentHTML('beforeend', `<a href="${post.link}"><button >${post.text}</button></a>`);
-      });
-    })
-    .catch((error) => console.error("Error fetching menu:", error));
+//   fetch("./components/navbar.json")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       console.log(navHeader);
+
+//       data.forEach((post) => {
+//         navHeader.insertAdjacentHTML('beforeend', `<a href="${post.link}"><button >${post.text}</button></a>`);
+//       });
+//     })
+//     .catch((error) => console.error("Error fetching menu:", error));
+// });
+
+let pageArray = [];
+const navHeader = document.getElementById("headernav");
+
+$.ajax({
+  method: 'GET',
+  url: './components/navbar.json',
+  success: function (response) {
+    pageArray = response;
+
+    response.forEach((post) => {
+      navHeader.insertAdjacentHTML('beforeend', `<a href="${post.link}"><button >${post.text}</button></a>`);
+    });
+  }
 });
