8c959bd04f6babe9b49ed370ec11b596b9a0a02c
john thornton <jb087011@gmail.com>
2025-09-10 00:00:08 -0400

    Draft up Project 1 story

--- itcs5122/scripts/implement_json.js
+++ itcs5122/scripts/implement_json.js
@@ -1,9 +1,11 @@
+const {Children} = require('react');
+
 let pages = [];
-const navbar = document.getElementById('headernav');
+const navbar = document.getElementById('navbar');
 
 $.ajax({
 	method: 'GET',
-	url: './components/navbar.json',
+	url: '../components/navbar.json',
 	success: function (response) {
 		pages = response;
 
